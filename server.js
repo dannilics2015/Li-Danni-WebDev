5d10377002c25488d2b7d5390730e66b1004c3e0
dannilics2015 <LiDanni@Danni-Li.local>
2016-09-18 16:25:52 -0700

    webdev

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// configure a public directory to host static content
+app.use(express.static(__dirname + '/public'));
+
+require ("./test/app.js")(app);
+
+var ipaddress = '127.0.0.1';
+var port = 3000;
+
+app.listen(port, ipaddress);
