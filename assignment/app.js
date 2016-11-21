c4e53e85ec3260b289b2b6135cd57a7719ecc38a
dannili <dannylee8812@gmail.com>
2016-11-20 22:21:10 -0800

    server

--- assignment/app.js
+++ assignment/app.js
@@ -10,5 +10,68 @@ module.exports = function(app) {
     require("./services/widget.service.server.js")(app, model);
 
 
+
+
+    app.get("/api/test", findAllMessages);
+    app.post("/api/test", createMessage);
+    app.delete("/api/test/:id", deleteMessage);
+
+    var connectionString = 'mongodb://localhost:27017/test';
+    if(process.env.MLAB_DB_USERNAME) {
+        connectionString = process.env.MLAB_DB_URL_INIT +
+            process.env.MLAB_DB_USERNAME + ":" +
+            process.env.MLAB_DB_PASSWORD +
+            process.env.MLAB_DB_URL_END + '/' +
+            process.env.MLAB_DB_NAME;
+    }
+
+    var mongoose = require("mongoose");
+    mongoose.connect(connectionString);
+
+    var TestSchema = mongoose.Schema({
+        message: String
+    });
+
+    var TestModel = mongoose.model("TestModel", TestSchema);
+
+    function findAllMessages(req, res) {
+        TestModel
+            .find()
+            .then(
+                function(tests) {
+                    res.json(tests);
+                },
+                function(err) {
+                    res.status(400).send(err);
+                }
+            );
+    }
+
+    function createMessage(req, res) {
+        TestModel
+            .create(req.body)
+            .then(
+                function(test) {
+                    res.json(test);
+                },
+                function(err) {
+                    res.status(400).send(err);
+                }
+            );
+    }
+
+    function deleteMessage(req, res) {
+        TestModel
+            .remove({_id: req.params.id})
+            .then(
+                function(result) {
+                    res.json(result);
+                },
+                function(err) {
+                    res.status(400).send(err);
+                }
+            );
+    }
+
 };
 
